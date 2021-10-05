e55c7bfdd4ca6db5e0180831158d610c2d3a30e9
Jose Jimenez <josejimenezjr18@gmail.com>
2021-10-05 09:41:08 -0700

    Update Maze_Demo.ino

--- examples/Maze_Demo/Maze_Demo.ino
+++ examples/Maze_Demo/Maze_Demo.ino
@@ -1,9 +1,28 @@
+#include <Maze.h>
+
+Maze maze( 16, 16 );
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  int row = 15;
+  int column = 15;
+  
+  if( maze.outOfBounds(row, column) ) {
+    Serial.println("out of bounds"); 
+    row = column = 0;
+  }
+  
+  MazeCell * cell = maze.at(row, column);
+  for( MazeCell * openNeighbor : cell->getNeighborList() ) {
+    /* visit cell neighbors and change their distance */
+    if( openNeighbor->visited ) continue;
+	  openNeighbor->setVisited( true );
+	  openNeighbor->distance = cell->distance + 1;
+  }
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // for more inspiration take a look at https://github.com/jimenezjose/Micromouse_Firmware_2019/
 }
